4893cbba7ce35708820f81e5634225e8f1a5761a
Adam Jurik <adam.jurik@grip-digital.com>
2023-01-24 14:44:54 +0100

    EFishState c++

---
+++ Source/FishAi/Fish/EFishState.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EFishState : uint8 
+{
+	Idle     UMETA(DisplayName = "Idle"),
+	Bait     UMETA(DisplayName = "Bait"),
+	Danger   UMETA(DisplayName = "Danger"),	
+};

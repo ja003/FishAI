24fdc9735751ab2dc117f73fdf795857ee5df1fe
Adam Jurik <adam.jurik@grip-digital.com>
2023-02-02 16:40:52 +0100

    throw predict c++ - begin

---
+++ Source/FishAi/PlayerObjects/ThrowPrediction.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThrowPrediction.h"
+
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Called every frame
+void AThrowPrediction::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AThrowPrediction::CreateSpline()
+{
+	SplineComponent = CreateDefaultSubobject<USplineComponent>("Spline");
+	if(SplineComponent)
+	{
+		SetRootComponent(SplineComponent);  
+	}
+
+	EndPoint = GetWorld()->SpawnActor(EndPointBP->GetClass());
+	EndPoint->SetHidden(true);
+	
+}
+
+void AThrowPrediction::DestroySpline()
+{
+	if(SplineComponent == nullptr) return;
+
+	SplineComponent->DestroyComponent();
+	SplineComponent = nullptr;
+	EndPoint->Destroy();
+	EndPoint = nullptr;
+	
+}
+
+void AThrowPrediction::DestroyMeshes()
+{
+	for (auto splineMesh : SplineMeshes)
+	{
+		splineMesh->DestroyComponent();
+	}
+	SplineMeshes.Empty();
+}
+
+void AThrowPrediction::DrawSpline()
+{
+	if(SplineComponent == nullptr) return;
+
+	FPredictProjectilePathParams params;
+	params.StartLocation = Start;
+	params.LaunchVelocity = Velocity;
+	params.ActorsToIgnore = IgnoredActors;
+	params.ProjectileRadius = 25;
+	FPredictProjectilePathResult predictResult;
+	UGameplayStatics::PredictProjectilePath(GetWorld(), params, predictResult);
+
+	// ...
+}
---
+++ Source/FishAi/PlayerObjects/ThrowPrediction.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ThrowPrediction.generated.h"
+
+class USplineMeshComponent;
+class USplineComponent;
+UCLASS()
+class FISHAI_API AThrowPrediction : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	void CreateSpline();
+
+	void DestroySpline();
+
+	void DestroyMeshes();
+
+	void DrawSpline();
+
+	void SetEnabled(bool bEnabled);
+
+	void UpdateValues();
+
+	UPROPERTY(VisibleAnywhere, Category = "Spline")
+	USplineComponent* SplineComponent;
+
+public:
+	
+	TArray<USplineMeshComponent*> SplineMeshes;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> EndPointBP;
+
+	AActor* EndPoint;
+
+	FVector Start;
+	FVector Velocity;
+
+	bool bIsEnabled;
+
+	TArray<AActor*> IgnoredActors;
+	
+};

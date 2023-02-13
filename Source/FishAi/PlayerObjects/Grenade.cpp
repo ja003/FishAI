095ff51bc950eced2ec7ac312f4f0c0982e6463d
Adam Jurik <adam.jurik@grip-digital.com>
2023-02-13 10:37:31 +0100

    added grenade object

---
+++ Source/FishAi/PlayerObjects/Grenade.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grenade.h"
+
+void AGrenade::OnEnteredWater()
+{
+	Super::OnEnteredWater();
+
+	UE_LOG(LogTemp, Log, TEXT("xxx explode"));
+}
---
+++ Source/FishAi/PlayerObjects/Grenade.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ThrowableObject.h"
+#include "UObject/Object.h"
+#include "Grenade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHAI_API AGrenade : public AThrowableObject
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void OnEnteredWater() override;
+};
